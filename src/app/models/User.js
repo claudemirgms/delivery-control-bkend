672fb323a8ea9adafb3a18369f1aca018f84351f
Claudemir Gomes <claudemirgms@gmail.com>
2021-12-13 17:18:30 -0300

    add unity_id in user model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     email: String,
     password: String,
-    confirmed: Boolean,   
+    confirmed: Boolean,
+    unity_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Unity'
+    }
 });
 
 module.exports = mongoose.model("User", UserSchema);
